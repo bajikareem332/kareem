2babe31edcce706a513779431f8af255e8d32f07
bajikareem332 <shaikbajikareem@gmail.com>
2024-11-19 16:41:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,3 +8,9 @@ def buildartifact()
 {
  sh'mvn package'
 }
+
+
+def deploytoTomcat(jobname,ipaddress,context)
+{
+ sh "scp /var/lib/jenkins/workspace/sharedlibreries/target/helloworld.war ubuntu@172.31.16.164:/var/lib/tomcat10/webapps/newtest.war"
+}
