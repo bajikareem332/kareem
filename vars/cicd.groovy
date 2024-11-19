6e6833771c759e53e5c7d88ba0a61d8096008698
bajikareem332 <shaikbajikareem@gmail.com>
2024-11-19 17:13:15 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,20 +1,4 @@
  def gitdownload(repo)
  {
-  git "https://github.com/sudarshansw7/${repo}.git"
+    git "https://github.com/sudarshansw7/${repo}.git"
  } 
-
-
-def buildartifact()
-{
- sh'mvn package'
-}
-
-
-def deployTomcat(jobname,ipaddress,context)
-{
- sh "scp /var/lib/jenkins/workspace/${jobname}/target/helloworld.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context}.war"
-}
-
-
-
-
